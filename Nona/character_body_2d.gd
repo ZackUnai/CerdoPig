763bf2ac3ec5b05f4597e7dedfa53064085060e5
TwiztedEsquizo <mario.radolcato@gmail.com>
2025-06-09 16:51:51 -0500

    sexo
    pon play sin ver nada

--- Nona/character_body_2d.gd
+++ Nona/character_body_2d.gd
@@ -1,1 +1,32 @@
 extends CharacterBody2D
+
+var spd = 50
+
+func _process(delta):
+	
+	var movet : Vector2
+	
+	if Input.is_key_pressed(KEY_A):
+		movet.x -= 1
+		
+	
+	if Input.is_key_pressed(KEY_D):
+		movet.x += 1
+		
+	
+	if Input.is_key_pressed(KEY_S):
+		movet.y += 1
+		
+	
+	if Input.is_key_pressed(KEY_W):
+		movet.y -= 1
+		
+	
+	velocity.x = movet.x * spd
+	velocity.y = movet.y * spd
+	
+	move_and_slide()
+	
+	if Input.is_key_pressed(KEY_Q):
+		queue_free()
+		
